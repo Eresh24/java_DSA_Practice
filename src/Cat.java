c8b2509d12652b818a85683dc786c57820ba0a79
Iresh Maharjan <iresh.maharjan24@gmail.com>
2024-09-18 19:06:39 +1000

    new changes

---
+++ src/Cat.java
@@ -0,0 +1,14 @@
+class Cat {
+    // Field (variable)
+    int number;
+
+    // Constructor
+    Cat(int number) {
+        this.number = number;
+    }
+
+    // Method to print the value
+    void printNumber() {
+        System.out.println("Number: " + number);
+    }
+}
